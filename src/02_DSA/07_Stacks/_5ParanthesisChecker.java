4559cdac99d4dad9728492fa430aa2faf2f1a9bb
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-04-05 22:58:12 +0530

    Stack parenthesis checker

--- src/02_DSA/07_Stacks/_5ParanthesisChecker.java
+++ src/02_DSA/07_Stacks/_5ParanthesisChecker.java
@@ -1,3 +1,47 @@
+import java.util.*;
+import java.util.Stack;
+
+class Solution {
+    public boolean isParenthesisBalanced(String s) {
+
+        Stack<Character> st = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if(ch == '{' || ch == '(' || ch =='[') {
+                st.push(ch);
+            }
+            else {
+
+                if(st.isEmpty()) return false;
+
+                if((st.peek() == '{' && ch == '}') ||
+                   (st.peek() == '(' && ch ==')') ||
+                    (st.peek() == '[' && ch == ']')) {
+                        st.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
 public class _5ParanthesisChecker {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // taking input
+        System.out.println("Enter string : ");
+        String s = sc.nextLine();
+
+        // function call
+        Solution sol = new Solution();
+        boolean result = sol.isParenthesisBalanced(s);
+        System.out.println(result);
+
+    }
 }
